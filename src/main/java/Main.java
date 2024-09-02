ce644d34d1c44f5b260f857663c6e430b7b7716b
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-09-02 12:33:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite seu nome: ");
+    String nome = sc.next();
+    System.out.println("Digite sua senha: ");
+    int senha = sc.nextInt();
+    if(nome.equals("Aline") && senha == 123)
+    {
+     System.out.println("Bem vindo!");
+    }
+    
+sc.close();
+    
+  }
+}
